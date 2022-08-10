b1c81cc9f978cadb83a96a7fae27b7be8f125ce3
malaijeyan <malaiarasi2001@gmail.com>
2022-08-10 15:04:10 -0700

    newly added app,index files

---
+++ App.js
@@ -0,0 +1,10 @@
+ import './App.css';
+ import Addquantity from './Addquantity';
+ function App(){
+    return(
+        <div className="app">
+            <Addquantity />
+        </div>
+    );
+ }
+ export default App;
